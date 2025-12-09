7ade20957296b95e56666fc0edbd408fedd59ed0
Joseph Afolabi <maverickoluwatomisin@gmail.com>
2025-12-09 18:48:15 +0100

    feat: replace addUserToDB with syncUserToDatabase for user synchronization in layout

---
+++ app/utils/actions.ts
@@ -0,0 +1,24 @@
+"use server";
+
+import prisma from "./db";
+import { KindeUser } from "@kinde-oss/kinde-auth-nextjs";
+
+export async function syncUserToDatabase(user: KindeUser) {
+  try {
+    const existingUser = await prisma.user.findUnique({
+      where: { id: user.id },
+    });
+
+    if (!existingUser) {
+      await prisma.user.create({
+        data: {
+          id: user.id,
+          email: user.email || "",
+          name: user.given_name + " " + user.family_name,
+        },
+      });
+    }
+  } catch (error) {
+    console.error("Error syncing user:", error);
+  }
+}
