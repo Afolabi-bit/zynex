eb764264a10b4f34a8d7b22b678fb24c2b36c793
Joseph Afolabi <maverickoluwatomisin@gmail.com>
2025-12-10 14:25:57 +0100

    feat: introduce Prisma ORM with User, Domain, and TestMetric models, and add user synchronization utility.

--- app/utils/actions.ts
+++ app/utils/actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import prisma from "./db";
+import prisma from "@/lib/db";
 import { KindeUser } from "@kinde-oss/kinde-auth-nextjs";
 
 export async function syncUserToDatabase(user: KindeUser) {
---
+++ lib/db.ts
@@ -0,0 +1,27 @@
+// lib/prisma.ts
+
+import { PrismaClient } from "./generated/prisma";
+import { PrismaNeon } from '@prisma/adapter-neon'
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
+
+// Create the Neon adapter with connection string
+const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL })
+
+// Initialize Prisma Client with the adapter
+ const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({ 
+    adapter,
+    log: process.env.NODE_ENV === 'development' 
+      ? ['query', 'error', 'warn'] 
+      : ['error'],
+  })
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
+}
+
+export default prisma
